3907a526e3a34b954b7b35d8ae727a6706a52459
CommonLoon102 <CommonLoon102@users.noreply.github.com>
2020-11-17 20:49:21 +0100

    Supress messages

---
+++ RegexFileSearcher/RegexFileSearcher/GlobalSuppressions.cs
@@ -0,0 +1,12 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "GUI element", Scope = "member", Target = "~F:RegexFileSearcher.MainForm.btnSelectAll")]
+[assembly: SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "GUI element", Scope = "member", Target = "~F:RegexFileSearcher.MainForm.btnSelectNone")]
+[assembly: SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "GUI element", Scope = "member", Target = "~F:RegexFileSearcher.MainForm.btnInvertSelection")]
+[assembly: SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "GUI element", Scope = "member", Target = "~F:RegexFileSearcher.MainForm.btnOpenSelected")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Code readability", Scope = "member", Target = "~M:RegexFileSearcher.MainForm.#ctor(System.Boolean)")]
